3d4812b2f34ad66587f93fddca39c2375cd38a2f
loli <l.c.yepes802jm@gmail.com>
2021-12-03 13:30:13 -0500

    primer comentario

---
+++ ejerciciofor.js
@@ -0,0 +1,49 @@
+for (var i = 0; i < 3; i++) {
+	document.write("maria<br>");
+	if (i==2){
+		for(var s = 0; s<3; s++){
+			document.write("<b>pedro<br></b>");
+			if (s==0){
+				for (var e = 0; e < 4; e++) {
+				document.write("nana<br>");
+				}
+			}
+			else if(s==2){
+				for (var e = 0; e < 2; e++) {
+				document.write("nana<br>");
+				}
+			}		
+		}
+	}
+}
+
+document.write("<br>");
+document.write("<hr></hr>");
+
+for (var i = 0; i < 3; i++) {
+	document.write("madero<br>");
+	if (i==2) {
+		for (var s = 0; s < 3; s++) {
+			document.write("beto <br>");
+			if (s==2) {
+				for (var e = 0; e < 4; e++) {
+					document.write("juan <br>");
+					if (e==3) {
+						for (var a = 0; a < 3; a++) {
+						document.write("pedro <br>");						
+							if (a==3) {
+								document.write("bertha");
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+
+
+
+
+
